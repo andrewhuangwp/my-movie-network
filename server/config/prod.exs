423f2998ec5f375bd0d92f271e9b0e10bae29e6e
andrewhuangwp <andrewhuangwp@gmail.com>
2021-04-10 19:29:14 -0400

    fix config

--- server/config/prod.exs
+++ server/config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :movie_api, MovieApiWeb.Endpoint,
-  url: [host: "my-movie-network.normalwebsite.art", port: 80],
+  url: [host: "my-movie-network.normalwebsite.art", port: 80]
 
 # Do not print debug messages in production
 config :logger, level: :info
